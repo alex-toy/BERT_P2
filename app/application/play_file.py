84402a5c4a45e0a1ac9bc3a21ef88f01fb3c4b0a
alessio rea <alexei.80@hotmail.fr>
2021-04-16 13:42:49 +0200

    progress application

---
+++ app/application/play_file.py
@@ -0,0 +1,9 @@
+from app.application.get_prediction import get_prediction
+
+
+
+
+if __name__ == "__main__":
+    
+    sentence = "This movie was pretty interesting."
+    get_prediction(sentence)
